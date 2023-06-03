d80d5815224b7838d47d87f316f66576e9b435c8
javieb5186 <javieb5186@gmail.com>
2023-06-02 18:18:20 -0600

    Add API request to index.html, images generate, alt text generate, p tag static content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,9 @@
-var breedUrl = "https://dogapi.dog/api/v2/breeds"
-var factsUrl = "https://dogapi.dog/api/v2/facts"
+var breedUrl = "https://dogapi.dog/api/v2/breeds";
+var factsUrl = "https://dogapi.dog/api/v2/facts";
 
-//fetch(breedUrl)
-//.then(function (response) {
- //   return response.json();
-//  })
- // .then(function (data) {
-//    console.log(data);
-//  });
+const factEl = document.getElementById("fact");
 
-const factEl = document.getElementById("fact")
+const imgs = document.getElementsByClassName("img");
  
 fetch(factsUrl)
 .then(function (response) {
@@ -18,5 +12,33 @@ fetch(factsUrl)
   .then(function (data) {
     factEl.textContent = data.data[0].attributes.body
   });
+
+const apiKEY = "5Ng5KCn+rvjkpNagvsh0Cg==iqEQFjzT9ajCB9np";
+const request = "https://api.api-ninjas.com/v1/dogs?shedding=3&barking=3&energy=3";
+
+fetch(request, {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': apiKEY
+  }
+})
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+
+  console.log(data);
+  let random1 = Math.floor(Math.random() * 20);
+  let random2;
+  do {
+    random2 = Math.floor(Math.random() * 20);
+  } while (random2 === random1);
+
+
+  imgs[0].setAttribute("src", data[random1].image_link);
+  imgs[0].setAttribute("alt", data[random1].name);
+  imgs[1].setAttribute("src", data[random2].image_link);
+  imgs[1].setAttribute("alt", data[random2].name);
+})
 
   
