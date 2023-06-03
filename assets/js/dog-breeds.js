3e6fff33fcce59396ababfb0a7d4166864bf47e9
Chris Harris <cHarris505@gmail.com>
2023-06-03 10:22:49 -0600

    added comments to dog-breeds.js.

--- assets/js/dog-breeds.js
+++ assets/js/dog-breeds.js
@@ -1,5 +1,6 @@
 const apiKEY = "5Ng5KCn+rvjkpNagvsh0Cg==iqEQFjzT9ajCB9np";
 const request = "https://api.api-ninjas.com/v1/dogs?shedding=3&barking=3&energy=3";
+var url = 'https://bootcamp-apiproxy.herokuapp.com/token/72b8b6de-0089-4257-a4b4-02d655ad67f6';
 
 // Section Elements
 var breedList = document.getElementById('breedSelection');
@@ -18,8 +19,7 @@ var trainability = document.getElementById('trainability');
 var breedBtn = document.querySelectorAll('.breed');
 
 var target;
-
-// Buttons
+// Fetch request for SELECT YOUR BREED and BREED FACTS
 fetch(request, {
     method: 'GET',
     headers: {
@@ -31,6 +31,7 @@ fetch(request, {
 })
 .then(function (data) {
     console.log(data);
+    // Creates new list items based on breed name, applies the class breed, and appends breed names as list items.
     for (var i = 0; i < data.length; i++) {
         var listItem = document.createElement('li');
         listItem.setAttribute('class', 'breed btn');
@@ -39,11 +40,13 @@ fetch(request, {
         breedList.appendChild(listItem);
     }
 
+    // On click, calls the function setStats to each of the breed name buttons built above
     breedList.addEventListener("click", function(event) {
       target = event.target.id;
       setStats();
     });
 
+    // changes the breed stats depending on the breed name button clicked.
     function setStats() {
       for (var i = 0; i < data.length; i++) {
         if(data[i].name === target)
@@ -60,3 +63,5 @@ fetch(request, {
       }
     } 
 });
+
+fet
